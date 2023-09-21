f466fbd34448e574bf95da20a27c9cbdf2b6e04e
YeongWoooo <rdd9223@gmail.com>
2023-09-21 15:44:15 +0900

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World test"}
+		res := &response{Message: "Hello World test2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
